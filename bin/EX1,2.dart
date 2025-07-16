b9f8b3108cad9463ba344a92a9e8876f9e66d50f
SHadya_Fathy <shadyafathy80@gmail.com>
2025-07-16 23:42:53 +0300

    Add EX1,2 practice

---
+++ bin/EX1,2.dart
@@ -0,0 +1,20 @@
+// //What is the purpose of the ? operator in Dart null safety?
+// // Make the variable nullable ,  contain a value or null
+// void main() {
+//   String? name = null;  
+//   print(name);         
+
+//   name = "Shadya";
+//   print(name);      
+// }
+
+
+
+
+//Create a late variable named address, assign a US value to it and print it.
+void main() {
+  late String address;
+
+  address = "US";
+  print(address);
+}
