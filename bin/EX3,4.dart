28a96eb194de94cb1b878e181b1d660baf17fd63
SHadya_Fathy <shadyafathy80@gmail.com>
2025-07-17 00:04:51 +0300

    Add EX3,4 practice

---
+++ bin/EX3,4.dart
@@ -0,0 +1,9 @@
+//How do you declare a nullable type in Dart null safety?
+// declare a nullable type by adding ? after the type name
+// String? name = null;
+
+//Write a program in a dart to create an age variable and assign a null value to it using ?
+void main() {
+  int? age = null;
+  print("Age is: $age");
+}
